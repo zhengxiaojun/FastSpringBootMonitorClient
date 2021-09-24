4516204bac4d8d901ed2a22b0c78dd6c253829bc
zhengxiaojun <zhengxiaojun@vcredit.com>
2021-09-24 10:53:39 +0800

    first spring boot monitor client demo

---
+++ src/main/java/com/xiaojun/MySpringBootMonitorClient/MySpringBootMonitorClientApplication.java
@@ -0,0 +1,13 @@
+package com.xiaojun.MySpringBootMonitorClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootMonitorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootMonitorClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaojun/MySpringBootMonitorClient/MySpringBootMonitorClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaojun.MySpringBootMonitorClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootMonitorClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
